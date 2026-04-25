fdac3d218f2b3632d4bce30ace5319fe5030f586
Alzbeta Kulichova <kulichova.alzbeta@gmail.com>
2026-04-25 18:16:14 +0200

    Add OnWindowClosed event and WindowName property

--- MFFGamejam2026Summer/Assets/Scripts/WindowController.cs
+++ MFFGamejam2026Summer/Assets/Scripts/WindowController.cs
@@ -2,9 +2,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-
-// Create State working, idle
-
 public enum WindowState
 {
     Idle,
@@ -16,8 +13,12 @@ public class WindowController : MonoBehaviour
     [SerializeField] TMP_Text titleText;
     [SerializeField] RectTransform clientArea;
     [SerializeField] Button closeButton;
+
     WindowState _state = WindowState.Idle;
-    
+
+    public string WindowName { get; private set; }
+    public event System.Action<WindowController> OnWindowClosed;
+
     // ----------------------------------------------------------------
     // Factory
     // ----------------------------------------------------------------
@@ -28,6 +29,7 @@ public class WindowController : MonoBehaviour
         GameObject contentPrefab)
     {
         var win = Instantiate(prefab, parent);
+        win.WindowName = title;
         win.titleText.text = title;
         win.SpawnContent(contentPrefab);
         return win;
@@ -40,24 +42,29 @@ public class WindowController : MonoBehaviour
     {
         closeButton.onClick.AddListener(Close);
     }
-    
-    public void SetTitle(string title) => titleText.text = title;
 
-    public void Close() => Destroy(gameObject);
-    
-    public WindowState GetState()  => _state;
+    public void SetTitle(string title)
+    {
+        WindowName = title;
+        titleText.text = title;
+    }
+
+    public void Close()
+    {
+        OnWindowClosed?.Invoke(this);
+        Destroy(gameObject);
+    }
+
+    public WindowState GetState() => _state;
     public void SetState(WindowState state) => _state = state;
-    
+
     public RectTransform GetClientArea() => clientArea;
     public RectTransform GetCloseButtonRect() => closeButton != null ? closeButton.transform as RectTransform : null;
 
     void SpawnContent(GameObject contentPrefab)
     {
         if (contentPrefab == null) return;
-
         Instantiate(contentPrefab, clientArea);
-
-        // Force the layout to recalculate immediately
         LayoutRebuilder.ForceRebuildLayoutImmediate(transform as RectTransform);
     }
 }

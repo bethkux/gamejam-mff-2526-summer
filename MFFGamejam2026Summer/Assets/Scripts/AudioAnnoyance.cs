02b853f75b0cc0dc5a8fd28406b363a2082182c6
Martin Strupek <martin.strupek@enehano.cz>
2026-04-25 17:23:01 +0200

    apply stash, keep both prefabs

---
+++ MFFGamejam2026Summer/Assets/Scripts/AudioAnnoyance.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class AudioAnnoyance : MonoBehaviour
+{
+    public string SFX = "Error";
+    public float delayForNext = 0.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        AudioManager.Instance.PlaySFX(SFX);
+        
+    }
+    private void OnEnable()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

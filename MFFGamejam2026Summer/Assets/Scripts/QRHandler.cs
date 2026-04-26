ef8fca8bf4d20c7a2282d91a97d81d7fbfc36f4c
Martin Strupek <martin.strupek@enehano.cz>
2026-04-26 13:03:20 +0200

    QR updated

--- MFFGamejam2026Summer/Assets/Scripts/QRHandler.cs
+++ MFFGamejam2026Summer/Assets/Scripts/QRHandler.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QRHandler : MonoBehaviour
 {
+    public Button YesButton;
+    public Button NoButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        YesButton.onClick.AddListener(stuff);
+        NoButton.onClick.AddListener(stuff);
     }
 
     // Update is called once per frame
@@ -13,4 +17,11 @@ public class QRHandler : MonoBehaviour
     {
         
     }
+
+    void stuff()
+    {
+        Debug.Log("QR button was pressed");
+        Destroy(gameObject);
+    }
+
 }
